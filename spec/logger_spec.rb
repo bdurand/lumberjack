c17c49959e92ba9d8b2c2b4d1e62b5be75c56a55
Devin Christensen <quixoten@gmail.com>
2014-01-16 16:33:27 -0700

    Make lumberjack compatible with stdlib logger
    The Logger from the ruby standard library will use the program name as
    the message if both a message and block are missing.
    
    From the docs
      `progname`
        Program name string. Can be omitted. Treated as a message if no
        `message` and `block` are given.

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -17,56 +17,56 @@ describe Lumberjack::Logger do
       logger = Lumberjack::Logger.new(output)
       logger.device.class.should == Lumberjack::Device::Writer
     end
-    
+
     it "should open a file path in a device" do
       logger = Lumberjack::Logger.new(File.join(tmp_dir, "log_file_1.log"))
       logger.device.class.should == Lumberjack::Device::LogFile
     end
-    
+
     it "should use the null device if the stream is :null" do
       logger = Lumberjack::Logger.new(:null)
       logger.device.class.should == Lumberjack::Device::Null
     end
-    
+
     it "should set the level with a numeric" do
       logger = Lumberjack::Logger.new(:null, :level => Lumberjack::Severity::WARN)
       logger.level.should == Lumberjack::Severity::WARN
     end
-    
+
     it "should set the level with a level" do
       logger = Lumberjack::Logger.new(:null, :level => :warn)
       logger.level.should == Lumberjack::Severity::WARN
     end
-    
+
     it "should default the level to INFO" do
       logger = Lumberjack::Logger.new(:null)
       logger.level.should == Lumberjack::Severity::INFO
     end
-    
+
     it "should set the progname"do
       logger = Lumberjack::Logger.new(:null, :progname => "app")
       logger.progname.should == "app"
     end
-    
+
     it "should create a thread to flush the device" do
       Thread.should_receive(:new)
       logger = Lumberjack::Logger.new(:null, :flush_seconds => 10)
     end
   end
-  
+
   context "attributes" do
     it "should have a level" do
       logger = Lumberjack::Logger.new
       logger.level = Lumberjack::Severity::DEBUG
       logger.level.should == Lumberjack::Severity::DEBUG
     end
-    
+
     it "should have a progname" do
       logger = Lumberjack::Logger.new
       logger.progname = "app"
       logger.progname.should == "app"
     end
-    
+
     it "should be able to silence the log in a block" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :buffer_size => 0, :level => Lumberjack::Severity::INFO, :template => ":message")
@@ -79,7 +79,7 @@ describe Lumberjack::Logger do
       logger.info("four")
       output.string.split.should == ["one", "three", "four"]
     end
-    
+
     it "should be able to customize the level of silence in a block" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :buffer_size => 0, :level => Lumberjack::Severity::INFO, :template => ":message")
@@ -93,7 +93,7 @@ describe Lumberjack::Logger do
       logger.info("four")
       output.string.split.should == ["one", "woof", "four"]
     end
-    
+
     it "should not be able to silence the logger if silencing is disabled" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :buffer_size => 0, :level => Lumberjack::Severity::INFO, :template => ":message")
@@ -107,7 +107,7 @@ describe Lumberjack::Logger do
       logger.info("four")
       output.string.split.should == ["one", "two", "three", "four"]
     end
-    
+
     it "should be able to set the progname in a block" do
       logger = Lumberjack::Logger.new
       logger.set_progname("app")
@@ -120,7 +120,7 @@ describe Lumberjack::Logger do
       block_executed.should == true
       logger.progname.should == "app"
     end
-    
+
     it "should only affect the current thread when silencing the logger" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :buffer_size => 0, :level => Lumberjack::Severity::INFO, :template => ":message")
@@ -144,7 +144,7 @@ describe Lumberjack::Logger do
         status = 2
       end
     end
-    
+
     it "should only affect the current thread when changing the progname in a block" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :progname => "thread1", :buffer_size => 0, :level => Lumberjack::Severity::INFO, :template => ":progname :message")
@@ -169,7 +169,7 @@ describe Lumberjack::Logger do
       end
     end
   end
-  
+
   context "flushing" do
     it "should autoflush the buffer if it hasn't been flushed in a specified number of seconds" do
       output = StringIO.new
@@ -184,7 +184,7 @@ describe Lumberjack::Logger do
       sleep(0.15)
       output.string.split(Lumberjack::LINE_SEPARATOR).should == ["message 1", "message 2", "message 3"]
     end
-    
+
     it "should write the log entries to the device on flush and update the last flushed time" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :level => Lumberjack::Severity::INFO, :template => ":message", :buffer_size => 32767)
@@ -195,7 +195,7 @@ describe Lumberjack::Logger do
       output.string.split(Lumberjack::LINE_SEPARATOR).should == ["message 1"]
       logger.last_flushed_at.should >= last_flushed_at
     end
-    
+
     it "should flush the buffer and close the devices" do
       output = StringIO.new
       logger = Lumberjack::Logger.new(output, :level => Lumberjack::Severity::INFO, :template => ":message", :buffer_size => 32767)
@@ -206,7 +206,7 @@ describe Lumberjack::Logger do
       output.should be_closed
     end
   end
-  
+
   context "logging" do
     let(:output){ StringIO.new }
     let(:device){ Lumberjack::Device::Writer.new(output, :buffer_size => 0) }
@@ -242,28 +242,37 @@ describe Lumberjack::Logger do
       end
       output.string.should == "[2011-01-30T12:31:56.123 INFO block(#{$$}) #] test#{n}"
     end
-    
+
+    it "should add entries with a progname but no message or block" do
+      time = Time.parse("2011-01-30T12:31:56.123")
+      Time.stub!(:now).and_return(time)
+      logger.set_progname("default") do
+        logger.add(Lumberjack::Severity::INFO, nil, "message")
+      end
+      output.string.should == "[2011-01-30T12:31:56.123 INFO default(#{$$}) #] message#{n}"
+    end
+
     it "should add entries with a block" do
       time = Time.parse("2011-01-30T12:31:56.123")
       Time.stub!(:now).and_return(time)
       logger.add(Lumberjack::Severity::INFO){"test"}
       output.string.should == "[2011-01-30T12:31:56.123 INFO test(#{$$}) #] test#{n}"
     end
-    
+
     it "should log entries (::Logger compatibility)" do
       time = Time.parse("2011-01-30T12:31:56.123")
       Time.stub!(:now).and_return(time)
       logger.log(Lumberjack::Severity::INFO, "test")
       output.string.should == "[2011-01-30T12:31:56.123 INFO test(#{$$}) #] test#{n}"
     end
-    
+
     it "should append messages with unknown severity to the log" do
       time = Time.parse("2011-01-30T12:31:56.123")
       Time.stub!(:now).and_return(time)
       logger << "test"
       output.string.should == "[2011-01-30T12:31:56.123 UNKNOWN test(#{$$}) #] test#{n}"
     end
-    
+
     it "should ouput entries to STDERR if they can't be written the the device" do
       stderr = $stderr
       $stderr = StringIO.new
@@ -278,17 +287,17 @@ describe Lumberjack::Logger do
         $stderr = stderr
       end
     end
-    
+
     context "log helper methods" do
       let(:device){ Lumberjack::Device::Writer.new(output, :buffer_size => 0, :template => ":message") }
-      
+
       it "should only add messages whose severity is greater or equal to the logger level" do
         logger.add(Lumberjack::Severity::DEBUG, "debug")
         logger.add(Lumberjack::Severity::INFO, "info")
         logger.add(Lumberjack::Severity::ERROR, "error")
         output.string.should == "info#{n}error#{n}"
       end
-    
+
       it "should only log fatal messages when the level is set to fatal" do
         logger.level = Lumberjack::Severity::FATAL
         logger.fatal("fatal")
@@ -304,7 +313,7 @@ describe Lumberjack::Logger do
         logger.unknown("unknown")
         output.string.should == "fatal#{n}unknown#{n}"
       end
-    
+
       it "should only log error messages and higher when the level is set to error" do
         logger.level = Lumberjack::Severity::ERROR
         logger.fatal("fatal")
@@ -320,7 +329,7 @@ describe Lumberjack::Logger do
         logger.unknown("unknown")
         output.string.should == "fatal#{n}error#{n}unknown#{n}"
       end
-    
+
       it "should only log warn messages and higher when the level is set to warn" do
         logger.level = Lumberjack::Severity::WARN
         logger.fatal("fatal")
@@ -336,7 +345,7 @@ describe Lumberjack::Logger do
         logger.unknown("unknown")
         output.string.should == "fatal#{n}error#{n}warn#{n}unknown#{n}"
       end
-    
+
       it "should only log info messages and higher when the level is set to info" do
         logger.level = Lumberjack::Severity::INFO
         logger.fatal("fatal")
@@ -352,7 +361,7 @@ describe Lumberjack::Logger do
         logger.unknown("unknown")
         output.string.should == "fatal#{n}error#{n}warn#{n}info#{n}unknown#{n}"
       end
-    
+
       it "should log all messages when the level is set to debug" do
         logger.level = Lumberjack::Severity::DEBUG
         logger.fatal("fatal")
@@ -368,7 +377,7 @@ describe Lumberjack::Logger do
         logger.unknown("unknown")
         output.string.should == "fatal#{n}error#{n}warn#{n}info#{n}debug#{n}unknown#{n}"
       end
-  
+
       it "should only log unkown messages when the level is set above fatal" do
         logger.level = Lumberjack::Severity::FATAL + 1
         logger.fatal("fatal")
