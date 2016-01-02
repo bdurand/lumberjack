bc5e3437193eaacd96168a3f5636a64455c3c087
Brian Durand <bbdurand@gmail.com>
2016-01-02 08:36:09 -0800

    remove unassigned variable

--- lib/lumberjack/device/size_rolling_log_file.rb
+++ lib/lumberjack/device/size_rolling_log_file.rb
@@ -39,7 +39,7 @@ module Lumberjack
 
       def roll_file?
         @manual || stream.stat.size > @max_size
-      rescue SystemCallError => e
+      rescue SystemCallError
         false
       end
       
